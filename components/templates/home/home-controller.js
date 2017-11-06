140cd0072ab6eebe9fca40730bd5707f880550b2
Will Bullock <will@obviouslee.com>
2017-11-06 15:00:58 -0500

    parallaz/css animations; footer elements

--- components/templates/home/home-controller.js
+++ components/templates/home/home-controller.js
@@ -18,7 +18,7 @@ function HomeController($scope, $state, store, $window, contentful, $http) {
 				return $sce.trustAsResourceUrl(mediaUrl);
 			};
 		}
-	// 
+	//
 	// contentful.entries('content_type=homePage').then(function(res) {
 	// 	// console.log('home page contentful data',res);
 	// 	$scope.home = res.data.items[0];
@@ -45,3 +45,51 @@ function HomeController($scope, $state, store, $window, contentful, $http) {
 	// });
 
 }
+
+// // makes the parallax elements
+// function parallaxIt() {
+//
+//   // create variables
+//   var $fwindow = $(window);
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//
+//   // on window scroll event
+//   $fwindow.on('scroll resize', function() {
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   });
+//
+//   // for each of content parallax element
+//   $('[data-type="content"]').each(function (index, e) {
+//     var $contentObj = $(this);
+//     var fgOffset = parseInt($contentObj.offset().top);
+//     var yPos;
+//     var speed = ($contentObj.data('speed') || 0 );
+//
+//     $fwindow.on('scroll resize', function (){
+//       yPos = fgOffset - scrollTop / speed;
+//
+//       $contentObj.css('top', yPos);
+//     });
+//   });
+//
+//   // for each of background parallax element
+//   $('[data-type="background"]').each(function(){
+//     var $backgroundObj = $(this);
+//     var bgOffset = parseInt($backgroundObj.offset().top);
+//     var yPos;
+//     var coords;
+//     var speed = ($backgroundObj.data('speed') || 0 );
+//
+//     $fwindow.on('scroll resize', function() {
+//       yPos = - ((scrollTop - bgOffset) / speed);
+//       coords = '40% '+ yPos + 'px';
+//
+//       $backgroundObj.css({ backgroundPosition: coords });
+//     });
+//   });
+//
+//   // triggers winodw scroll for refresh
+//   $fwindow.trigger('scroll');
+// };
+//
+// parallaxIt();
