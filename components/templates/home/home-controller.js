b1f87b37ca9638ac5a0b0f7d90d85fa3ef3b7291
hsandstromOM <hosea@obviouslee.com>
2017-11-22 15:23:07 -0500

    changed banner-caption width to 31vh

--- components/templates/home/home-controller.js
+++ components/templates/home/home-controller.js
@@ -55,9 +55,8 @@ function HomeController($scope, $state, store, $window, contentful, $http) {
 	var wh = window.innerHeight;
 
 	new ScrollMagic.Scene({
-	  offset: wh*3
+	  offset: wh*2
 	})
-	.setClassToggle("section#four", "is-active")
 	.addTo(ctrl);
 	//
 	// contentful.entries('content_type=homePage').then(function(res) {
@@ -86,51 +85,3 @@ function HomeController($scope, $state, store, $window, contentful, $http) {
 	// });
 
 }
-
-// // makes the parallax elements
-// function parallaxIt() {
-//
-//   // create variables
-//   var $fwindow = $(window);
-//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//
-//   // on window scroll event
-//   $fwindow.on('scroll resize', function() {
-//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   });
-//
-//   // for each of content parallax element
-//   $('[data-type="content"]').each(function (index, e) {
-//     var $contentObj = $(this);
-//     var fgOffset = parseInt($contentObj.offset().top);
-//     var yPos;
-//     var speed = ($contentObj.data('speed') || 0 );
-//
-//     $fwindow.on('scroll resize', function (){
-//       yPos = fgOffset - scrollTop / speed;
-//
-//       $contentObj.css('top', yPos);
-//     });
-//   });
-//
-//   // for each of background parallax element
-//   $('[data-type="background"]').each(function(){
-//     var $backgroundObj = $(this);
-//     var bgOffset = parseInt($backgroundObj.offset().top);
-//     var yPos;
-//     var coords;
-//     var speed = ($backgroundObj.data('speed') || 0 );
-//
-//     $fwindow.on('scroll resize', function() {
-//       yPos = - ((scrollTop - bgOffset) / speed);
-//       coords = '40% '+ yPos + 'px';
-//
-//       $backgroundObj.css({ backgroundPosition: coords });
-//     });
-//   });
-//
-//   // triggers winodw scroll for refresh
-//   $fwindow.trigger('scroll');
-// };
-//
-// parallaxIt();
